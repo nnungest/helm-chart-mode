3f158ba235c98e3d10c9baffee63ce5a68686012
Nick Nungester <nnungest@gmail.com>
2025-02-22 12:38:51 -0500

    chore: initialize repo

---
+++ lisp/helm-chart-mode.el
@@ -0,0 +1,23 @@
+;;; helm-chart-mode.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Nick Nungester
+;;
+;; Author: Nick Nungester <nnungest@gmail.comm>
+;; Maintainer: Nick Nungester <nnungest@gmail.com>
+;; Created: February 22, 2025
+;; Modified: February 22, 2025
+;; Version: 0.0.1
+;; Keywords: tools
+;; Homepage: https://github.com/nnungest/helm-chart-mode
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(provide 'helm-chart-mode)
+;;; helm-chart-mode.el ends here
